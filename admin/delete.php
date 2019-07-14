6c117728fbc60af66a5e24dec7e2c0818b84a81c
Maaduu <mamadubalde644@yahoo.com>
2019-07-15 00:08:21 +0200

    add delete.php file

---
+++ admin/delete.php
@@ -0,0 +1,66 @@
+<?php
+    // Faire appel à la BDD
+    require 'database.php';
+
+    if(!empty($_GET['id']))
+    {
+        $id = checkInput($_GET['id']);
+    }
+
+    if(!empty($_POST))
+    {
+        $id = checkInput($_POST['id']);
+        // Conecter à la BDD
+        $db = Database::connect();
+        //Elimination de  l'item  à la BDD
+        $statement = $db->prepare("DELETE FROM items WHERE id = ?");
+        $statement->execute(array($id));
+
+        // Disconecter à la BDD
+        Database::disconnect();
+        // Redirection to index.php
+        header("Location: index.php");
+    }
+
+    // Securisation de données
+    function checkInput($data)
+    {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Burger</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+
+    <body>
+        <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger <span class="glyphicon glyphicon-cutlery"></span></h1>
+         <div class="container admin">
+            <div class="row">
+                <h1><strong>Supprimer un item</strong></h1>
+                <br>
+                <form class="form" action="delete.php" role="form" method="post">
+                    <input type="hidden" name="id" value="<?php echo $id;?>"/>
+                    <p class="alert alert-warning">Etes vous sur de vouloir supprimer ?</p>
+                    <div class="form-actions">
+                      <button type="submit" class="btn btn-warning">Oui</button>
+                      <a class="btn btn-default" href="index.php">Non</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
+
